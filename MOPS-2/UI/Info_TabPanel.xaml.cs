91b48fdea43bb3f98d5d6088f4c85c099659903a
Nimblefaf <nimblefaf@gmail.com>
2023-02-21 12:33:06 +0500

    Added text for keyboard shortcuts and beats glossary

--- MOPS-2/UI/Info_TabPanel.xaml.cs
+++ MOPS-2/UI/Info_TabPanel.xaml.cs
@@ -26,6 +26,51 @@ namespace MOPS.UI
         {
             InitializeComponent();
             InfoTextBlock1.Text = "0x40 Hues of C#, V" + Assembly.GetExecutingAssembly().GetName().Version.ToString().Remove(3);
+            glossaryTextBox.Text =
+                "x  Vertical blur, image and colour (snare)\r\n" +
+                "o  Horizontal blur, image and colour (bass)\r\n" +
+                "-  Change image and colour (no blur)\r\n" +
+                ":  Colour only\r\n" +
+                "*  Image only\r\n" +
+                "X  Vertical blur only\r\n" +
+                "O  Horizontal blur only\r\n" +
+                "+¤ Blackout/whiteout (classic)\r\n" +
+                "|! Blackout/whiteout (short)\r\n" +
+                //"┊¡ Blackout/whiteout (instant)\r\n" +
+                //"▼▽ Blackout/whiteout (fade out)\r\n" +
+                //"▲△ Blackout/whiteout (fade in)\r\n" +
+                //")( Trippy cirle in/out and change image\r\n" +
+                //">< Trippy cirle in/out\r\n" +
+                "~  Fade colour\r\n" +
+                "=  Fade and change image\r\n" +
+                "i  Invert all colours\r\n" +
+                "I  Invert & change image\r\n";
+                //"ı  Fade invert\r\n" +
+                //"sv Horizontal/vertical slice\r\n" +
+                //"SV Horizontal/vertical slice and change image\r\n" +
+                //"# Double slice\r\n" +
+                //"@ Double slice and change image\r\n" +
+                //"←↓↑→ Shutter\r\n" +
+                //"¯ Stop all effects in bank immediately\r\n" +
+                //"_ Stop timed effects (fade, slice etc)";
+            shortcutsTextBox.Text =
+                "↑↓ Change song\r\n" +
+                "←→ Change image\r\n" +
+                "[N] Random song\r\n" +
+                "-+ Change volume\r\n" +
+                "[M] Toggle mute\r\n" +
+                "[B] Restart song from build\r\n" +
+                //"[F] Toggle automode\r\n" +
+                "[H] Toggle UI hide\r\n" +
+                "[C] Character list\r\n" +
+                "[S] Song list\r\n" +
+                "[W] Toggle window\r\n" +
+                "[R] Resource packs\r\n" +
+                "[L] Load local zip\r\n" +
+                "[O] Options\r\n" +
+                "[I] Information\r\n" +
+                "[1-6] Change UI\r\n" +
+                "[Alt+Enter] Fullscreen";
         }
 
         private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
